a9d35f2a6132468f19c9f074a7b842760c524026
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2024-05-08 16:25:46 +0300

    Split the visualization function into multiple functions

--- src/visualization.py
+++ src/visualization.py
@@ -5,9 +5,9 @@ import warnings
 from config import IMG_PATH
 
 
-def visualize_eeg(raw_data, times=None, show_plots=True, verbose=True):
-    file_name, raw = raw_data
-    img_path = f'{IMG_PATH}/{file_name}'
+def plot_eeg(raw_data, show=True, verbose=True):
+    idx, raw = raw_data
+    img_path = f'{IMG_PATH}/{idx}'
 
     plt.close('all')
 
@@ -20,6 +20,17 @@ def visualize_eeg(raw_data, times=None, show_plots=True, verbose=True):
     if verbose:
         print(f"Raw data plot saved as '{img_path}_raw_data_plot.png'.")
 
+    if show:
+        plt.show()
+    else:
+        plt.close('all')
+
+def plot_psd(raw_data, show=True, verbose=True):
+    idx, raw = raw_data
+    img_path = f'{IMG_PATH}/{idx}'
+
+    plt.close('all')
+
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore')
         with mne.utils.use_log_level('ERROR'):
@@ -29,14 +40,24 @@ def visualize_eeg(raw_data, times=None, show_plots=True, verbose=True):
     if verbose:
         print(f"PSD plot saved as '{img_path}_psd_plot.png'.")
 
-    if times is not None:
-        evoked = raw.average()
-        fig_topo = evoked.plot_topomap(times, size=3, show=False)
-        fig_topo.savefig(f'{img_path}_topomap.png')
-        if verbose:
-            print(f"Topomap saved as '{img_path}_topomap.png'.")
+    if show:
+        plt.show()
+    else:
+        plt.close('all')
 
-    if show_plots:
+def plot_topomap(raw_data, times, show=True, verbose=True):
+    idx, raw = raw_data
+    img_path = f'{IMG_PATH}/{idx}'
+
+    plt.close('all')
+
+    evoked = raw.average()
+    fig_topo = evoked.plot_topomap(times, size=3, show=False)
+    fig_topo.savefig(f'{img_path}_topomap.png')
+    if verbose:
+        print(f"Topomap saved as '{img_path}_topomap.png'.")
+
+    if show:
         plt.show()
     else:
         plt.close('all')
