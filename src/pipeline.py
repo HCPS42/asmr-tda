a48fe95481f88593351f56340e83de5fef4ee390
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2024-05-11 20:00:38 +0300

    Add Execution Time

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,8 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
+import warnings
+import time
 
 from config import CLASSIFIER
 from data import get_training_data
@@ -27,10 +29,18 @@ def get_pipeline(df):
     return pipeline
 
 def run_experiment(save_result=False):
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
+    start_time = time.time()
+
     X_train, X_val, y_train, y_val = get_training_data()
 
     pipeline = get_pipeline(X_train)
 
     pipeline.fit(X_train, y_train)
 
-    evaluate_pipeline(pipeline, X_train, X_val, y_train, y_val, save_result)
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+
+    evaluate_pipeline(pipeline, X_train, X_val, y_train, y_val, execution_time, save_result)
