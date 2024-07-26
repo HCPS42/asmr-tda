eec6c918a94eb4f371ca23f753923286f93c9747
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2024-07-26 13:49:53 +0300

    revived-sound-25

--- src/model.py
+++ src/model.py
@@ -7,17 +7,19 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(64*30*2, 1024)
+        self.bn1 = nn.BatchNorm1d(1024)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(1024, 512)
+        self.bn2 = nn.BatchNorm1d(512)
         self.fc3 = nn.Linear(512, num_classes)
 
     def forward(self, x):
         x = x.to(torch.float)
         x = self.flatten(x)
-        x = self.relu(self.fc1(x))
+        x = self.bn1(self.relu(self.fc1(x)))
         x = self.dropout(x)
-        x = self.relu(self.fc2(x))
+        x = self.bn2(self.relu(self.fc2(x)))
         x = self.dropout(x)
         x = self.fc3(x)
         return x
