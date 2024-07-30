a98e3ed9d9f97a00517b2b0f71666f1acd2c13d6
Temirkhan Zimanov <zimanovtemirkhan@gmail.com>
2024-07-31 00:47:23 +0300

    spring-rain-30

--- src/model.py
+++ src/model.py
@@ -1,17 +1,26 @@
 import torch
 import torch.nn as nn
 
+
 class Model(nn.Module):
     def __init__(self, num_classes):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=64, out_channels=8, kernel_size=60, stride=1)
-        self.fc1 = nn.Linear(8, num_classes)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64*30*2, 32)
+        self.bn1 = nn.BatchNorm1d(32)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(32, 8)
+        self.bn2 = nn.BatchNorm1d(8)
+        self.fc3 = nn.Linear(8, num_classes)
 
     def forward(self, x):
         x = x.to(torch.float)
-        x = x.view(x.size(0), x.size(1), -1)
-        x = self.relu(self.conv1(x))
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
+        x = self.flatten(x)
+        x = self.bn1(self.relu(self.fc1(x)))
+        x = self.dropout(x)
+        x = self.bn2(self.relu(self.fc2(x)))
+        x = self.dropout(x)
+        x = self.fc3(x)
         return x
+    
